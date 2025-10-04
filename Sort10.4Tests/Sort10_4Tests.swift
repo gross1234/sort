7244016b3b4d6114ef8ad6f5a1fadd89d0b0f6a0
gross1234 <ross.gabem@gmail.com>
2025-10-04 10:38:25 -0500

    Initial Commit

---
+++ Sort10.4Tests/Sort10_4Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Sort10_4Tests.swift
+//  Sort10.4Tests
+//
+//  Created by Gabe Ross on 10/4/25.
+//
+
+import Testing
+@testable import Sort10_4
+
+struct Sort10_4Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Sort10.4UITests/Sort10_4UITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Sort10_4UITestsLaunchTests.swift
+//  Sort10.4UITests
+//
+//  Created by Gabe Ross on 10/4/25.
+//
+
+import XCTest
+
+final class Sort10_4UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
